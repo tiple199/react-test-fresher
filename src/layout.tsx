8ea70a2876b0fa62332d20210d9d177f203994e5
Tiep <lenhotiep1992004@gmail.com>
2025-07-25 09:36:00 +0700

    init project

---
+++ src/layout.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Layout() {
+
+  return (
+    <div>
+       Le Nho Tiep
+    </div>
+  )
+}
+
+export default Layout
